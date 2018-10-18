ca3d7f080866b35470f1eb69fd79c9fe01c969b1
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 23:45:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  def alphabetize(phrases)
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  phrases.sort_by do |phrase|
+    phrase.chars.map { |c| alpha.index(c) }
+  end
+end
 end
